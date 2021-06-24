cd414c1f793ca3aa8d49a4331056eb9123efeae4
Veli Can Mert <veli.mert@ug.bilkent.edu.tr>
2021-06-24 10:51:57 +0300

    Admin can list all the users and links for forms of the selected user have been added, forms list page will be created for admins.

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { showAllUsers } from '../services/Services';
+import { Link } from 'react-router-dom';
+import './Users.css';
 
 function Users() {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    showAllUsers().then(res => {
+      setUsers(res);
+    });
+  }, []);
+
+  const onClickHandler = username => {
+    console.log('username', username);
+  };
+
+  return (
+    <div className='users'>
+      {users.map(user => (
+        <div className='users'>
+          <div className='username'>{user.username}</div>
+          <div>{user.identificationNo}</div>
+          <div>{user.status}</div>
+          <Link to={`/forms`} onClick={() => onClickHandler(user.username)}>
+            <span>Forms</span>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Users;
